90833b078b786fcf5d36b1f898a4e2787affb3e0
Atlant5000 <maximmmd13@gmail.com>
2024-11-03 22:47:39 +0200

    ComputerPractical6 Version1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+            /*Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}+{b}={a+b}");
+            Console.Read();*/
+
+            //task 2
+            /*Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}+{b}={b}+{a}");
+            Console.Read();*/
+
+            //Task 3
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            double product = a * b;
+            Console.WriteLine($"{a}*{b}={product:F1}");
+            Console.Read();*/
+
+            //Task 4 
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine($"({a:F2}+{b:F2})+{c:F2}={a:F2}+({b:F2}+{c:F2})");
+            Console.Read();*/
+
+            //Task 5
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write("d=");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}/{b}+{c}/{d}={(a / b + c / d):F2}");
+            Console.Read();*/
+
+            //task 6
+            /*Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write($"a+b={a}+{b}={a + b}");
+            Console.Write($"  a-b={a}-{b}={a - b}");
+            Console.Write($"  a*b={a}*{b}={a * b}");
+            Console.Read();*/
+
+            //Task 7
+            /*Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+            int c = int.Parse(Console.ReadLine());
+            Console.Write($"[{a}*({b}+{c})]=[({b}+{c})*{a}]={a*(b+c)}");
+            Console.Read();*/
+
+            //Task 8
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c=");
+            double c = double.Parse(Console.ReadLine());
+            Console.Write($"({a:F4}+({b:F4}+{c:F4})=({a:F4}+{b:F4}+{c:F4})");
+            Console.Read();*/
+
+            //Task 9
+            /*Console.Write("x=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("y=");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write($"{x * 3}/y={(x * 3 / y):F4}");
+            Console.Read();*/
+
+            //Task10
+            /*Console.Write("a=");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write($"a*b={a:F3}*{b:F3}={a*b:F3}   a*b={a:F5}*{b:F5}={a*b:F5}");
+            Console.Read();*/
+        }
+    }
+}
